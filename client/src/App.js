4d5ebe3bb2dae43cc37ce278aa494b0b15bb3151
Richard Dixon <rvictordixon@gmail.com>
2022-03-05 02:42:07 -0500

    add grommet for styling, add main and sidebar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Grid, Button, DataTable, Sidebar, TextInput } from "grommet";
+import { Search } from "grommet-icons";
 
 function App() {
+  const [sort, setSort] = React.useState({
+    property: "name",
+    direction: "desc",
+  });
+
+  const columns = [
+    { property: "id", header: "bug" },
+    { property: "title", header: "" },
+    { property: "status", header: "status" },
+    { property: "created", header: "created" },
+    { property: "due", header: "due" },
+    { property: "reporter", header: "reporter" },
+  ];
+  const DATA = [
+    {
+      id: "PRJ-1",
+      title: "handle null issues",
+      status: "completed",
+      created: "02-02-2022",
+      due: "03-10-2022",
+      reporter: "Richard Dixon",
+    },
+    {
+      id: "PRJ-1",
+      title: "handle null issues",
+      status: "completed",
+      created: "02-02-2022",
+      due: "03-10-2022",
+      reporter: "Richard Dixon",
+    },
+    {
+      id: "PRJ-1",
+      title: "handle null issues",
+      status: "completed",
+      created: "02-02-2022",
+      due: "03-10-2022",
+      reporter: "Richard Dixon",
+    },
+    {
+      id: "PRJ-1",
+      title: "handle null issues",
+      status: "completed",
+      created: "02-02-2022",
+      due: "03-10-2022",
+      reporter: "Richard Dixon",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Grid
+        rows={["xxsmall", "medium", "xsmall"]}
+        columns={["small", "auto"]}
+        areas={[
+          ["header", "header"],
+          ["sidebar", "main"],
+          ["footer", "footer"],
+        ]}
+        gap="small"
+      >
+        <Box background="brand" gridArea="header">
+          Header
+        </Box>
+
+        <Sidebar background="light-5" gridArea="sidebar">
+          <Grid gap="small">
+            <Button color="dark-1" hoverIndicator="light-1" primary>
+              Milestones
+            </Button>
+            <Button color="dark-1" hoverIndicator="light-1" primary>
+              Bugs
+            </Button>
+            <Button color="dark-1" hoverIndicator="light-1" primary>
+              Timesheet
+            </Button>
+            <Button color="dark-1" hoverIndicator="light-1" primary>
+              Feed
+            </Button>
+          </Grid>
+        </Sidebar>
+
+        <Box pad="small" background="light-2" gridArea="main">
+          <Grid gap="small" columns={["auto", "medium", "small"]}>
+            <h1 style={{ margin: 0 }}>My Bugs</h1>
+            <TextInput icon={<Search />} placeholder="search ..." />
+            <Button color="dark-1" hoverIndicator="light-1" primary>
+              Submit Bug
+            </Button>
+          </Grid>
+          <DataTable
+            columns={columns}
+            data={DATA}
+            sort={sort}
+            onSort={setSort}
+          />
+        </Box>
+
+        <Box background="dark-2" gridArea="footer">
+          Footer
+        </Box>
+      </Grid>
+    </Box>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { Grommet } from "grommet";
+import ReactDOM from "react-dom";
+import { aruba } from "./theme";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Grommet theme={aruba}>
+      <App />
+    </Grommet>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
