319f1fa50e6b5d9ef9e5aa1cb94ea37ba9908a33
Richard Dixon <rvictordixon@gmail.com>
2022-03-12 02:08:26 -0500

    Organize folder structure and add controller

---
+++ server/controllers/controllers.js
@@ -0,0 +1,5 @@
+//const express = require("express")
+
+exports.getAllTasks = (req, res) => {
+  res.send("Ok");
+};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+let corsOption = {
+  origin: `http://localhost:${port}`,
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello there!!");
-});
+const db = require("./models/index");
+db.sequelize.sync();
+
+const { getAllTasks } = require("./controllers/controllers");
+
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/tasks", getAllTasks);
+// app.get("/tasks/:id", getTask);
+// app.post("/task", postTask);
+// app.put("/tasks/:id", updateTask);
+// app.delete("/tasks/:id", deleteTask);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
